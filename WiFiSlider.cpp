ea80fc5190a7f80d91d2f240537ee5f91dce6d3c
mariuszmaximus <mariusz.maximus@gmail.com>
2025-01-14 23:57:58 +0100

    fix

--- WiFiSlider.cpp
+++ WiFiSlider.cpp
@@ -1,2 +1,2 @@
-#include "customslider.h"
+#include "WiFiSlider.h"
 
--- exampleWiFi/examplewifi.h
+++ exampleWiFi/examplewifi.h
@@ -6,7 +6,7 @@
 #include "WifiScanner.h"
 #include "WifiNetworkModel.h"
 
-#include "customslider.h"
+#include "WiFiSlider.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
