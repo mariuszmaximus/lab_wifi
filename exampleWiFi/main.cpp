61f3da1d7280e35016eac66378ffa8d5b7b5d0d0
max <max@draminski.com>
2025-01-14 09:47:12 +0100

    fix

--- exampleWiFi/main.cpp
+++ exampleWiFi/main.cpp
@@ -6,7 +6,10 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     ExampleWifi w;
+#ifdef WIN32
     w.resize(1024, 768);
-    w.show();
+#else    
+    w.showFullScreen();
+#endif
     return a.exec();
 }
